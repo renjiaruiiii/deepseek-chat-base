58d734b5555b6d3f716b5569792e0b4c7a5d660d
jiaruii <3389370260@qq.com>
2025-11-18 15:26:30 +0800

    更换了deepseek-ai/DeepSeek-V3-0324模型

--- src/services/deepseekApi.js
+++ src/services/deepseekApi.js
@@ -8,7 +8,7 @@ export const fetchDeepseekResponse = async (message) => {
                 'Authorization': 'Bearer sk-84i3AD6d32BrfqCO0F9NLN6kaO1ye1hU5gttw0k4xJUeCIvi' // 替换为你的 API 密钥
             },
             body: JSON.stringify({
-                model: 'gemini-2.0-flash-001',
+                model: 'deepseek-ai/DeepSeek-V3-0324',
                 messages: [{ role: 'user', content: message }],
                 stream: false
             })
